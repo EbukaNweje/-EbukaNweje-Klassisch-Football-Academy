a15a166f2ece1cdd463d79249a9223da56d4ee3c
Alabi Yusuf <yusufolatunjialabi@gmail.com>
2022-07-21 19:21:29 +0100

    Project Add

---
+++ JS/App.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector(".fa-bars")
+const menuList = document.querySelector(".menuList")
+const CloseDiv = document.querySelector(".MenuBar2")
+const closeMenu = document.querySelector(".fa-times")
+
+menu.onclick = ()=> {
+    menuList.style.display = 'flex'
+    closeMenu.style.display = 'block'
+    menu.style.display = 'none'
+    CloseDiv.style.display = 'flex'
+}
+
+closeMenu.onclick = ()=> {
+    menuList.style.display = 'none'
+    menu.style.display = 'flex'
+    closeMenu.style.display = 'none'
+    CloseDiv.style.display = 'none'
+    window.location.reload()
+}
